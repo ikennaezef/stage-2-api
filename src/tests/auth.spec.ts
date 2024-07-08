dbc960a53514d0f08df0f4bf9f6b58abfd9fb567
Ikenna <iykee0333@gmail.com>
2024-07-08 01:22:52 +0100

    tests

--- src/tests/auth.spec.ts
+++ src/tests/auth.spec.ts
@@ -1,33 +1,33 @@
 import request from "supertest";
 import { server } from "..";
 
-const newUser = {
-	firstName: "Test2 Firstname",
-	lastName: "Test2 Lastname",
-	email: "test2@mail.com",
-	password: "password123",
-	phone: "08604583",
-};
-
 describe("Authentication API Endpoints", () => {
 	let userID: any;
+	let newUser = {
+		firstName: "Test9 Firstname",
+		lastName: "Test9 Lastname",
+		email: "test9@mail.com",
+		password: "password123",
+		phone: "08604583",
+	};
 
 	afterAll(() => {
 		server.close();
 	});
 
-	beforeEach(() => {
-		jest.setTimeout(200000);
-	});
-
 	let token = "";
 
 	it("should create a new user", async () => {
-		const res = await request(server).post("/auth/register").send(newUser);
+		const res = await request(server).post("/auth/register").send({
+			firstName: newUser.firstName,
+			lastName: newUser.lastName,
+			email: newUser.email,
+			password: newUser.password,
+			phone: newUser.phone,
+		});
 
 		expect(res.statusCode).toBe(201);
-		userID = res.body?.data.user.userId;
-	});
+	}, 15000);
 
 	it("signup fails if there are missing fields", async () => {
 		const res = await request(server).post("/auth/register").send({
@@ -36,11 +36,18 @@ describe("Authentication API Endpoints", () => {
 
 		expect(res.statusCode).toBe(422);
 	});
+
 	it("signup fails if there is a duplicate email", async () => {
-		const res = await request(server).post("/auth/register").send(newUser);
+		const res = await request(server).post("/auth/register").send({
+			firstName: newUser.firstName,
+			lastName: newUser.lastName,
+			email: newUser.email,
+			password: newUser.password,
+			phone: newUser.phone,
+		});
 
 		expect(res.statusCode).toBe(422);
-	});
+	}, 15000);
 
 	it("should login the user", async () => {
 		const res = await request(server).post("/auth/login").send({
@@ -68,25 +75,25 @@ describe("Authentication API Endpoints", () => {
 		});
 
 		expect(res.statusCode).toBe(401);
-		// expect(res.body.title).toEqual("Test Book");
 	});
 
 	it("should get user organisations", async () => {
 		const res = await request(server)
 			.get("/api/organisations")
-			.auth(token, { type: "bearer" });
+			.set({ authorization: `Bearer ${token}` });
+
 		expect(res.statusCode).toEqual(200);
-	});
+	}, 10000);
 
 	it("should create a new organisation", async () => {
 		const res = await request(server)
-			.post("/auth/register")
+			.post("/api/organisations")
 			.send({
 				name: "Test Organisation",
 				description: "Test desc",
 			})
-			.auth(token, { type: "bearer" });
+			.set({ authorization: `Bearer ${token}` });
 
 		expect(res.statusCode).toBe(201);
-	});
+	}, 15000);
 });
